f2f7cec8f44e0206cb899a31767b69261e53559a
Bas Peeters <bas@peete.rs>
2019-10-12 08:58:51 +0200

    Install typescript and webpack

---
+++ webpack.config.ts
@@ -0,0 +1,23 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+const config = {
+    mode: 'development',
+    entry: path.join(__dirname, 'src', 'main.ts'),
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 3000,
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
+};
+
+export default config;
